71598bde4f7437d27d268e03137ad0a203593db3
ace-of-fades <alexandercharbonneau95@gmail.com>
2023-04-02 19:19:53 -0600

    Moon card animations

--- components/moon.jsx
+++ components/moon.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "../styles/Moons.module.css"
 
-
 const moon = ({name, radius, history}) => {
+
+  const front = (
+    <div><h1>{name}</h1><img className={styles.moonImg} src="/images/moon.png" alt="Moon"></img><h4>Radius: {radius} Miles</h4></div>
+  )
+  
+  const back = (
+    <div><p>{history}</p></div>
+  )
+
+const [side,setSide] = useState(front);
+const [css,setCss] = useState("");
+const [flipped,flip] = useState(true)
+
+const flipIt = () => {
+  if (flipped === true){
+    flip(false)
+    setCss("animate__animated animate__zoomOut animate__faster")
+    setTimeout(() => {
+      setSide(back)
+      setCss("animate__animated animate__zoomIn animate__faster")
+    }, "420");
+  }
+
+  if (flipped === false){
+    flip(true)
+    setCss("animate__animated animate__zoomOut animate__faster")
+    setTimeout(() => {
+      setSide(front)
+      setCss("animate__animated animate__zoomIn animate__faster")
+    }, "420");
+  }
+
+
+}
+
   return (
-    <div key={Math.random()} className={styles.moonCard}>
-        <h1>{name}</h1>
-        <img className={styles.moonImg} src="/images/moon.png" alt="Moon"></img>
-        
-        <h4>Radius: {radius} Miles</h4>
+    <div onClick={()=>flipIt()} key={Math.random()} className={`${styles.moonCard} ${css}`}>
+      {side}
     </div>
   )
 }
--- pages/planet/[planet]/moon-info.jsx
+++ pages/planet/[planet]/moon-info.jsx
@@ -1,6 +1,6 @@
 import Moon from '@/components/moon'
 import { useRouter } from 'next/router'
-import React, {useState, useMemo} from 'react'
+import React, {useState, useMemo, useEffect} from 'react'
 import styles from "../../../styles/Moons.module.css"
 const moon = () => {
     const router = useRouter()
@@ -13,12 +13,20 @@ const moon = () => {
         .then(data => setData(data))
       }, [planet])
 
+      useEffect(() => {
+        console.log(data?.result?.moons)
+      }, [data])
+      
+
   return (
     <>
     <div className={styles.moonContainer}>
     {data?.result?.moons?.map(b => (
         <Moon name={b.moonname} radius={b.moonRadiusMi} history={b.moonhistory}/>
     ))}
+
+      {data?.result?.moons.length === 0 ? <p className={styles.noMoons}>{planet.slice(0, 1).toUpperCase() + planet.slice(1)} has no moons!</p> : null }
+
     </div>
     </>
     
